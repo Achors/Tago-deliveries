399aeb4e28e83cf8e33a7312dc6a3cf18c7f6f17
Adolf <adolfnyerere@gmail.com>
2024-01-24 05:20:08 +0300

    added some frontend stuff
    added explore page
    modified my app.py in server
    
    next playing with routes in server

--- client/Tago/src/App.jsx
+++ client/Tago/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+import ExplorePage from './Products';
+import CartPage from './cartPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Explore</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart ({cart.length})</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Route exact path="/" render={() => <ExplorePage addToCart={addToCart} />} />
+        <Route path="/cart" render={() => <CartPage cart={cart} />} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
